0206cb693dc2f05838c2b6bd99b43d35e39c29d1
Jeremy <ohmycomput@gmail.com>
2019-12-10 19:39:01 +0100

    Add user control

--- src/Display/Bloc.js
+++ src/Display/Bloc.js
@@ -2,33 +2,27 @@ import React from "react";
 import "./Bloc.css";
 
 export default class Bloc extends React.Component {
-  constructor(props) {
-    super(props);
-    this.x = props.position.x;
-    this.y = props.position.y;
-    this.state = {
-      active: false
-    };
-  }
-
-  handleClic() {
-    // Handle the clic
-    this.setState({ active: !this.state.active });
-  }
-
   createClass() {
     // Change the class of the bloc
-    if (this.state.active) {
-      return "active";
+    switch (this.props.value) {
+      case 0:
+        return "";
+      case 1:
+        return "active";
+      default:
+        return "";
     }
-    return "";
   }
 
   render() {
-    console.log(`Render x:${this.x} y:${this.y}`);
+    const { x, y } = this.props.position;
+    // console.log(`Render x:${x} y:${y} v:${this.props.value}`);
     return (
       <div
-        onClick={() => this.handleClic()}
+        // When events are detected, call the parent function
+        onClick={event => this.props.handleClic(x, y, event)} // Event for click -> set the change
+        // Give the event to strop the propagation
+        onMouseEnter={() => this.props.handleEnter(x, y)} // If the mouse is inside a bloc
         className={"bloc " + this.createClass()}
       ></div>
     );
--- src/Display/Windows.js
+++ src/Display/Windows.js
@@ -1,34 +1,113 @@
 import React from "react";
 import Bloc from "./Bloc";
 
-const nbL = 6;
-const nbC = 5;
+const initialState = {
+  click: false, // To handle when the iser click
+  nbR: 15, // Number of rows
+  nbC: 20, // Number of columns
+  size: 50, // Size of the blocs
+  cells: [] // represent the board. Matrix of int
+};
 
 export default class Windows extends React.Component {
-  displayBlocs() {
-    // Display the grid of blocs
-    let arrayBlocs = []; // Contain row of blocs
-    let key = "";
-    for (let l = 0; l < nbL; l++) {
-      key = `r${l}`;
-      arrayBlocs.push(
-        <div className="row no-gutters" key={key}>
-          {this.addBlocs(nbC, key, l)}
-        </div>
-      );
-    }
-    return arrayBlocs;
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      ...initialState,
+      cells: this.initCells(initialState.nbR, initialState.nbC) // Current states of the board
+    };
   }
 
-  addBlocs(nbC, key, posiRow) {
-    // Add all blocs of a row
-    let arrayBlocs = [];
-    for (let i = 0; i < nbC; i++) {
-      arrayBlocs.push(
-        <Bloc key={`${key}C${i}`} position={{ x: i, y: posiRow }} />
-      );
+  initCells(nbRows, nbColumns) {
+    // Init all the cells at 0
+    let matrix = [];
+    let tmpRow = [];
+
+    for (let r = 0; r < nbRows; r++) {
+      tmpRow = [];
+      for (let c = 0; c < nbColumns; c++) {
+        tmpRow.push(0);
+      }
+      matrix.push(tmpRow);
     }
-    return arrayBlocs;
+    return matrix;
+  }
+
+  componentDidMount() {
+    // When the component mount, add event on click
+    // Handle when the user click outside the blocs
+    document.addEventListener("click", this.handleClic);
+    // document.addEventListener("mouseup", () => console.log("UP"));
+  }
+
+  componentWillUnmount() {
+    // When the component die, remove the event on click
+    document.removeEventListener("click", this.handleClic);
+  }
+
+  handleStateChange = (x, y) => {
+    // Function is call by the children
+    // If the user is clicking, change the value of the calling bloc
+    if (this.state.click) {
+      let nextState = this.state;
+
+      nextState.cells[y][x] = nextState.cells[y][x] === 0 ? 1 : 0;
+
+      this.setState(nextState);
+    }
+  };
+
+  handleUserClicInsideBloc = (x, y, clickEvent) => {
+    // Function is call by the children
+    // Handle is the user is clicking inside a bloc
+
+    // Stop the propagation of the event
+    // Prevent to get catch by the listener
+    clickEvent.stopPropagation();
+    clickEvent.nativeEvent.stopImmediatePropagation();
+
+    let nextState = this.state;
+
+    nextState.click = !nextState.click;
+    if (nextState.click) {
+      // We need to change the current value of the bloc
+      this.handleStateChange(x, y);
+    }
+
+    this.setState(nextState);
+  };
+
+  handleClic = () => {
+    // Handle is the user is clicking outside the blocs
+    console.log("CLIQUE");
+
+    let nextState = this.state;
+
+    nextState.click = !nextState.click;
+
+    this.setState(nextState);
+  };
+
+  displayBlocs() {
+    // return the board to display
+    return this.state.cells.map((arrayRow, indR) => (
+      // Iterate over a row
+      <div className="row nogutters" key={`R${indR}`}>
+        {arrayRow.map((value, indC) => {
+          // Iterate over the state of the cells
+          return (
+            <Bloc
+              key={`R${indR}C${indC}`}
+              position={{ x: indC, y: indR }}
+              value={value}
+              handleClic={this.handleUserClicInsideBloc}
+              handleEnter={this.handleStateChange}
+            />
+          );
+        })}
+      </div>
+    ));
   }
 
   render() {
