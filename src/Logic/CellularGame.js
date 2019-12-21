4c93bfb73317486a758bbc599719118184e9574b
Jeremy <ohmycomput@gmail.com>
2019-12-21 17:10:40 +0100

    Hierarchie

--- src/Logic/CellularGame.js
+++ src/Logic/CellularGame.js
@@ -4,37 +4,54 @@ import Windows from "../Display/Windows";
 import { Conway } from "./Algo/Conway";
 
 export default class CellularGame extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      m: [
+        [0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0]
+      ],
+      nM: [
+        [0, 0, 0, 0, 0],
+        [0, 1, 1, 0, 0],
+        [0, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0]
+      ]
+    };
+  }
+
   displayMatrix(m) {
     let val = m.reduce((p, c) => (p = `${p}${c}`));
     return <p>{val}</p>;
   }
 
-  render() {
-    const bob = [
-      [0, 1, 0, 0],
-      [0, 1, 1, 0],
-      [0, 1, 0, 0]
-    ];
-    let conway = new Conway(bob);
+  nextS() {
+    let nS = this.state;
+    let conway = new Conway(nS.nM);
 
     const m = conway.getMatrix();
 
     const nM = conway.next();
 
+    this.setState({
+      m: m,
+      nM: nM
+    });
+  }
+
+  render() {
     return (
       <div>
         <h1>Automate Cellulaire</h1>
-        <div>
-          {this.displayMatrix(m[0])}
-          {this.displayMatrix(m[1])}
-          {this.displayMatrix(m[2])}
-        </div>
+        <div>{this.state.m.map(el => this.displayMatrix(el))}</div>
         <hr />
         <div>
-          {this.displayMatrix(nM[0])}
-          {this.displayMatrix(nM[1])}
-          {this.displayMatrix(nM[2])}
+          <div>{this.state.nM.map(el => this.displayMatrix(el))}</div>
         </div>
+
+        <button onClick={() => this.nextS()}>Bob</button>
         {/* <Windows /> */}
         {/* <Controls /> */}
       </div>
